0d87300661c38fccda28a6be08bc3ff6e9cc60c3
ChenArno <517625126@qq.com>
2019-12-13 16:19:29 +0800

    修改模板

--- src/app.js
+++ src/app.js
@@ -4,21 +4,23 @@
  * @Author: chenArno
  * @Date: 2019-12-12 14:57:20
  * @LastEditors: chenArno
- * @LastEditTime: 2019-12-13 11:21:25
+ * @LastEditTime: 2019-12-13 14:20:56
  */
 
 import React from 'react'
 import './assets/css/index.less'
 import background from './assets/images/background.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>i have change react</h1>
-      <img src={background} />
-      {console.log('ccccccc')}
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>i have change {this.props.name}</h1>
+        <img src={background} />
+        {console.log('ccccccc')}
+      </div>
+    )
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: chenArno
  * @Date: 2019-12-12 15:05:48
  * @LastEditors: chenArno
- * @LastEditTime: 2019-12-12 15:06:26
+ * @LastEditTime: 2019-12-13 14:20:37
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(<App name="react" />, document.getElementById('root'))
