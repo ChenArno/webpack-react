deb1d4f4bd15d5a81f501f48fc18e9bbb47ac596
ChenArno <517625126@qq.com>
2019-12-15 10:57:22 +0800

    去除无效的打印日志

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -4,7 +4,7 @@
  * @Author: chenArno
  * @Date: 2019-12-12 14:59:42
  * @LastEditors: chenArno
- * @LastEditTime: 2019-12-13 10:20:22
+ * @LastEditTime: 2019-12-15 10:49:43
  */
 const merge = require('webpack-merge')
 const common = require('./webpack.common.config')
@@ -12,6 +12,7 @@ const path = require('path')
 
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'development',
@@ -23,7 +24,11 @@ module.exports = merge(common, {
     open: true,
     port: 8000,
     compress: true,
-    hot: true
+    hot: true,
+    // 如果使用webpack-dev-server，需要设为true，禁止显示devServer的console信息
+    quiet: true,
+    // 编译出现错误时，将错误直接显示在页面上
+    overlay: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -31,10 +36,17 @@ module.exports = merge(common, {
       inject: 'body',
       hash: false
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
     // HotModuleReplacementPlugin是webpack热更新的插件，
     // 设置devServer.hot为true，并且在plugins中引入HotModuleReplacementPlugin插件即可。
     // 还需要注意的是我们开启了hot，
     // 那么导出不能使用chunkhash，需要替换为hash。
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: [`Your application is running here:http://127.0.0.1:8000`]
+      },
+      // onErrors:,
+      clearConsole: true
+    })
   ]
 })
