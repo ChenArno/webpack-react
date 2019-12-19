5f7d7ed3efdd5e473ff1673210855e28fb3b69d4
ChenArno <517625126@qq.com>
2019-12-19 13:07:44 +0800

    新增绝对路径引用配置

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -1,12 +1,16 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: chenArno
  * @Date: 2019-12-12 14:53:30
- * @LastEditors: chenArno
- * @LastEditTime: 2019-12-13 11:20:34
+ * @LastEditors  : chenArno
+ * @LastEditTime : 2019-12-19 13:07:16
  */
-const path = require('path');
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
   entry: {
@@ -18,54 +22,55 @@ module.exports = {
     filename: 'js/bundle.js',
     path: path.resolve(__dirname, '../dist')
   },
+  resolve: {
+    // 设置别名
+    alias: {
+      '@': resolve('src'), // 这样配置后 @ 可以指向 src 目录
+      views: resolve('src/views'),
+      assets: resolve('src/assets')
+    }
+  },
   devtool: 'cheap-module-eval-source-map',
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      use: 'babel-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'postcss-loader'
-      ]
-    }, {
-      test: /\.less$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'postcss-loader',
-        'less-loader'
-      ]
-    }, {
-      test: /\.(sass|scss)$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'postcss-loader',
-        'sass-loader'
-      ]
-    }, {
-      test: /\.(jpg|png|gif)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'images/',
-          limit: 8192
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader']
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            limit: 8192
+          }
+        }
+      },
+      {
+        test: /\.(eot|ttf|svg|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath: 'font/'
+          }
         }
       }
-    }, {
-      test: /\.(eot|ttf|svg|woff|woff2)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name]_[hash].[ext]',
-          outputPath: 'font/'
-        }
-      }
-    }]
+    ]
   }
 }
