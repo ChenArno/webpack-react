8ae4347a52232a15adecc7516aff545f2834a91f
ChenArno <517625126@qq.com>
2019-12-23 10:44:19 +0800

    增加示例模板

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@
  * @Author: chenArno
  * @Date: 2019-12-12 15:05:48
  * @LastEditors  : chenArno
- * @LastEditTime : 2019-12-20 15:36:31
+ * @LastEditTime : 2019-12-23 10:44:03
  */
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './assets/css/index.less'
+// import background from '@/assets/images/background.jpg'
 
 console.log(process.env)
 ReactDOM.render(<h1>hello wrt-cli</h1>, document.getElementById('root'))
