9d80ada206180c670f6dabbce6e4bba49ffa34b0
ChenArno <517625126@qq.com>
2020-02-26 10:26:20 +0800

    引用assets

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@
  * @Author: chenArno
  * @Date: 2019-12-12 15:05:48
  * @LastEditors: chenArno
- * @LastEditTime: 2020-02-26 10:25:00
+ * @LastEditTime: 2020-02-26 10:26:07
  */
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import Home from 'pages/home.tsx'
-import './assets/css/index.less'
+import 'assets/css/index.less'
 // import background from '@/assets/images/background.jpg'
 
 console.log(process.env)
