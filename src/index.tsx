33cdda2d33d57e523fc5cf7d34640d868e2c84c9
ChenArno <517625126@qq.com>
2020-02-27 11:04:40 +0800

    增加path的相对位置设置

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,11 @@
  * @Author: chenArno
  * @Date: 2019-12-12 15:05:48
  * @LastEditors: chenArno
- * @LastEditTime: 2020-02-26 10:26:07
+ * @LastEditTime: 2020-02-27 11:04:32
  */
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import Home from 'pages/home.tsx'
+import Home from 'pages/home'
 import 'assets/css/index.less'
 // import background from '@/assets/images/background.jpg'
 
